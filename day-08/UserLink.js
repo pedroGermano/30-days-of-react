f090f78e0a329a3bd3ccdc67e2f1f283662cfe68
Pedro <pedrogermano232@gmail.com>
2021-01-05 16:10:52 -0300

    day-08 final code

---
+++ day-08/UserLink.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class UserLink extends React.Component {
+  render() {
+    const { user } = this.props;
+    return <div>Welcome back {user.name}</div>;
+  }
+}
+
+UserLink.propTypes = {
+  user: (props, propName, componentName) => {
+    if (!props[propName] || !props[propName].name) {
+      return new Error(
+        'Invalid ' +
+          propName +
+          ': No name property defined for component ' +
+          componentName
+      );
+    }
+  },
+};
+
+export default UserLink;
